3aa4432cb758004ad251878173422363e909beb5
Joseph24x7 <Joseph2471994@gmail.com>
2022-12-31 22:56:48 +0530

    Reverting to prev commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import React from 'react';
-import MyForm from '../resume.builder/MyForm';
+import MyForm from './resume.builder/MyForm';
 
 function App() {
   return (
