0da1751da9d8ff21e59a98734d21dd9432ba5b5e
joseph24x7 <joseph2471994@gmail.com>
2023-07-19 19:46:18 +0530

    Removing MyProfile and Homepage Css

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,10 +1,9 @@
 import React from "react";
-import { useNavigate } from "react-router-dom"; // Import the necessary hooks
-import "./HomePage.css";
+import { useNavigate } from "react-router-dom";
 
 const HomePage = (props) => {
   const { editedUserDetails } = props;
-  const navigate = useNavigate(); // Access the navigate function for navigation
+  const navigate = useNavigate();
 
   const handleBrowseProducts = () => {
     console.log("Browse Products clicked!");
@@ -23,22 +22,27 @@ const HomePage = (props) => {
   };
 
   return (
-    <div>
-      <h1 className="welcome-message">Welcome, {editedUserDetails ? editedUserDetails.name : "User"}!</h1>
-      <div className="dashboard">
-        <div className="dashboard-item" onClick={handleBrowseProducts}>
-          <h2>Browse Products</h2>
-          <p>Find your favorite products here.</p>
+    <div className="flex flex-col items-center justify-center h-screen">
+      <h1 className="text-4xl font-bold text-gray-600 my-10">
+        Welcome, {editedUserDetails ? editedUserDetails.name : "User"}!
+      </h1>
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-4 w-full max-w-3xl">
+        <div className="dashboard-item p-8 bg-white rounded-lg shadow-md border border-gray-300 cursor-pointer transform transition-transform hover:scale-105 hover:shadow-lg" onClick={handleBrowseProducts}>
+          <i className="text-4xl mb-4 text-blue-600 fas fa-shopping-cart"></i>
+          <h2 className="text-2xl font-bold text-gray-800">Browse Products</h2>
+          <p className="text-lg text-gray-600">Find your favorite products here.</p>
         </div>
 
-        <div className="dashboard-item" onClick={handleViewOrders}>
-          <h2>My Orders</h2>
-          <p>View your order history and status.</p>
+        <div className="dashboard-item p-8 bg-white rounded-lg shadow-md border border-gray-300 cursor-pointer transform transition-transform hover:scale-105 hover:shadow-lg" onClick={handleViewOrders}>
+          <i className="text-4xl mb-4 text-purple-600 fas fa-list-ul"></i>
+          <h2 className="text-2xl font-bold text-gray-800">My Orders</h2>
+          <p className="text-lg text-gray-600">View your order history and status.</p>
         </div>
 
-        <div className="dashboard-item" onClick={handleManageProfile}>
-          <h2>My Profile</h2>
-          <p>Manage your profile information.</p>
+        <div className="dashboard-item p-8 bg-white rounded-lg shadow-md border border-gray-300 cursor-pointer transform transition-transform hover:scale-105 hover:shadow-lg" onClick={handleManageProfile}>
+          <i className="text-4xl mb-4 text-green-600 fas fa-user"></i>
+          <h2 className="text-2xl font-bold text-gray-800">My Profile</h2>
+          <p className="text-lg text-gray-600">Manage your profile information.</p>
         </div>
       </div>
     </div>
--- src/components/MyProfile.js
+++ src/components/MyProfile.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from "react";
-import "./MyProfile.css"; // Import the CSS file for MyProfile component
 
 const MyProfile = ({ tokenResponse }) => {
   const [userDetails, setUserDetails] = useState(null);
@@ -83,85 +82,126 @@ const MyProfile = ({ tokenResponse }) => {
   };
 
   return (
-    <div className="profile-container">
-      <h2 className="profile-title">My Profile</h2>
+    <div className="profile-container bg-gray-100 p-5 rounded-lg shadow-md w-96 mx-auto">
+      <h2 className="profile-title text-center text-3xl font-semibold mb-5 text-blue-700">My Profile</h2>
       {isLoading ? (
-        <div className="loading-spinner"></div>
+        <div className="flex items-center justify-center mb-8">
+          <div className="loading-spinner border-4 border-solid border-blue-400 border-t-4 h-8 w-8 rounded-full animate-spin"></div>
+        </div>
       ) : userDetails ? (
         <div className="user-details">
-          <div className="form-group">
-            <label htmlFor="name">Name:</label>
+          <div className="form-group mb-4">
+            <label htmlFor="name" className="font-semibold text-gray-600">
+              Name:
+            </label>
             <input
               type="text"
               id="name"
               name="name"
               value={isEditing ? editedUserDetails.name : userDetails.name}
               onChange={handleInputChange}
-              className={isEditing ? "editable-input" : "read-only-input"}
+              className={`form-input ${isEditing ? "bg-white" : "bg-gray-200"} w-full px-4 py-2 rounded-md border ${
+                isEditing ? "border-blue-500 focus:border-blue-700" : "border-gray-300"
+              }`}
               readOnly={!isEditing}
             />
           </div>
-          <div className="form-group">
-            <label htmlFor="email">Email:</label>
-            <input type="email" id="email" value={userDetails.email} readOnly />
+          <div className="form-group mb-4">
+            <label htmlFor="email" className="font-semibold text-gray-600">
+              Email:
+            </label>
+            <input
+              type="email"
+              id="email"
+              value={userDetails.email}
+              className="form-input bg-gray-200 w-full px-4 py-2 rounded-md border border-gray-300"
+              readOnly
+            />
           </div>
-          <div className="form-group">
-            <label htmlFor="username">Username:</label>
+          <div className="form-group mb-4">
+            <label htmlFor="username" className="font-semibold text-gray-600">
+              Username:
+            </label>
             <input
               type="text"
               id="username"
               name="username"
               value={isEditing ? editedUserDetails.username : userDetails.username}
               onChange={handleInputChange}
-              className={isEditing ? "editable-input" : "read-only-input"}
+              className={`form-input ${isEditing ? "bg-white" : "bg-gray-200"} w-full px-4 py-2 rounded-md border ${
+                isEditing ? "border-blue-500 focus:border-blue-700" : "border-gray-300"
+              }`}
               readOnly={!isEditing}
             />
           </div>
-          <div className="form-group">
-            <label htmlFor="mobile">Mobile Number:</label>
+          <div className="form-group mb-4">
+            <label htmlFor="mobile" className="font-semibold text-gray-600">
+              Mobile Number:
+            </label>
             <input
               type="tel"
               id="mobile"
               name="mobile"
               value={isEditing ? editedUserDetails.mobile : userDetails.mobile}
               onChange={handleInputChange}
-              className={isEditing ? "editable-input" : "read-only-input"}
+              className={`form-input ${isEditing ? "bg-white" : "bg-gray-200"} w-full px-4 py-2 rounded-md border ${
+                isEditing ? "border-blue-500 focus:border-blue-700" : "border-gray-300"
+              }`}
               readOnly={!isEditing}
             />
           </div>
-          <div className="form-group">
-            <label htmlFor="gender">Gender:</label>
+          <div className="form-group mb-4">
+            <label htmlFor="gender" className="font-semibold text-gray-600">
+              Gender:
+            </label>
             {isEditing ? (
               <select
                 id="gender"
                 name="gender"
                 value={editedUserDetails.gender}
                 onChange={handleInputChange}
-                className="editable-input"
+                className="form-select w-full px-4 py-2 rounded-md border border-gray-300"
               >
                 <option value="male">Male</option>
                 <option value="female">Female</option>
                 <option value="other">Other</option>
               </select>
             ) : (
-              <input type="text" id="gender" value={userDetails.gender} readOnly />
+              <input
+                type="text"
+                id="gender"
+                value={userDetails.gender}
+                className="form-input bg-gray-200 w-full px-4 py-2 rounded-md border border-gray-300"
+                readOnly
+              />
             )}
           </div>
-          {isEditing ? (
-            <button className="profile-btn update-btn" onClick={handleUpdateClick}>
-              Update
-            </button>
-          ) : (
-            <button className="profile-btn edit-btn" onClick={handleEditClick}>
-              Edit
-            </button>
-          )}
+          <div className="flex justify-center mt-4">
+            {isEditing ? (
+              <button
+                className="profile-btn update-btn bg-blue-500 text-white px-4 py-2 rounded-md shadow-md hover:bg-blue-600"
+                onClick={handleUpdateClick}
+              >
+                Update
+              </button>
+            ) : (
+              <button
+                className="profile-btn edit-btn bg-green-500 text-white px-4 py-2 rounded-md shadow-md hover:bg-green-600"
+                onClick={handleEditClick}
+              >
+                Edit
+              </button>
+            )}
+          </div>
         </div>
       ) : (
-        <p className="error-message">{error || "Error fetching user details."}</p>
+        <p className="error-message text-red-500">
+          {error || "Error fetching user details."}
+        </p>
       )}
     </div>
   );
+  
 };
 
 export default MyProfile;
