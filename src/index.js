340d2b7375f02d1c2f61460e3e33a9d665a67cf0
joseph24x7 <joseph2471994@gmail.com>
2023-07-22 21:33:14 +0530

    code fixes

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import './index.css';
 import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <GoogleOAuthProvider clientId="103420507616-7a46i3etinqm6di5pon8h7aa7h0a8jng.apps.googleusercontent.com">
+    <GoogleOAuthProvider clientId="clientId">
         <React.StrictMode>
             <App />
         </React.StrictMode>
